786679d7e0db9c384594c5158273b36033e995c4
Daniel Rowe <danielrowemusic@gmail.com>
2019-06-08 11:36:51 -0700

    moved into hero to keep fullscreen

--- src/App.js
+++ src/App.js
@@ -5,30 +5,40 @@ import "./App.css";
 
 function App() {
   return (
-      <div className="container has-text-centered">
-        <div id="Harmonic">
-          <h2 className="subtitle is-4">harmonic series</h2>
+    <section className="hero is-white is-fullheight">
+      <div className="hero-head">
+        <h2 className="subtitle is-4 has-text-centered">harmonic series</h2>
+        <div className="container">
           <content>
             The harmonic series represent the naturally occurring pitches that
             can be created on an open string or tube. These intervals can be
             found in the partials on brass instruments or harmonics of string
-            instruments. Hover (or click on mobile) on any wave to hear it.
+            instruments and are the basis for our musical system. Hover over any
+            wave to hear it.
           </content>
-          <br />
-          <br />
-          <Wave harmonic="1" />
-          <Wave harmonic="2" />
-          <Wave harmonic="3" />
-          <Wave harmonic="4" />
-          <Wave harmonic="5" />
-          <Wave harmonic="6" />
-          <Wave harmonic="7" />
-          <Wave harmonic="8" />
-          <Wave harmonic="9" />
         </div>
-        <br/>
-        <p>&copy; 2019 <a href="https://danielrowetech.com">daniel rowe</a></p>
       </div>
+      <div className="hero-body">
+        <div className="container">
+          <div id="Harmonic">
+            <Wave harmonic="1" />
+            <Wave harmonic="2" />
+            <Wave harmonic="3" />
+            <Wave harmonic="4" />
+            <Wave harmonic="5" />
+            <Wave harmonic="6" />
+            <Wave harmonic="7" />
+            <Wave harmonic="8" />
+            {/* <Wave harmonic="9" /> */}
+          </div>
+        </div>
+      </div>
+      <div className="hero-footer has-text-centered">
+        <p>
+          &copy; 2019 <a href="https://danielrowetech.com">daniel rowe</a>
+        </p>
+      </div>
+    </section>
   );
 }
 
